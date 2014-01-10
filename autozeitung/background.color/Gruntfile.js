8600e0aed2e7e1f92b06f6ef5edb369998477c5f
Gregor Rackwitz <gregor.rackwitz@bauermedia.com>
2014-01-10 12:31:03 +0100

    added background.color.azo

--- autozeitung/background.color/Gruntfile.js
+++ autozeitung/background.color/Gruntfile.js
@@ -1,10 +1,20 @@
 module.exports = function(grunt) {
-  	'use strict';
-  	grunt.initConfig({
-  		pkg: grunt.file.readJSON('package.json'),
-  	});
+    'use strict';
 
-	var loadedGruntfile = require('../../_template/Gruntfile.js')(grunt);
-    
-    loadedGruntfile.registerTask('default', loadedGruntfile.taskList['default']);
+    require('../../_template/Gruntfile.js')(grunt);
+    var publishConfig = {
+      default: {
+               auth: {
+                  type: 'oauth',
+                  token: '9119f34cfa76b90db97388527b2171d69335bb3c'
+               },
+               file: {
+                  gistId: '<%= pkg.homepage %>',
+                  filename: '<%= pkg.name %>.js',
+                  file: 'build/<%= pkg.name %>.beautified.js'
+               }
+            }
+    };
+
+    grunt.config('publish',publishConfig);
 };
