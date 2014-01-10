6f490256c49eef98311f87312f5c2ab9f9d14640
Gregor Rackwitz <gregor.rackwitz@bauermedia.com>
2014-01-10 11:37:22 +0100

    added result script of build process

---
+++ autozeitung/background.click/build/azo.background.click.beautified.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+	function jAdInitBackground(){document.getElementsByTagName("body")[0].style.backgroundColor="#C0C0C0",document.getElementsByTagName("body")[0].style.cursor="pointer",document.getElementById("page").style.cursor="default",document.getElementsByTagName("body")[0].onclick=jAdBackgroundClickHandler}function jAdBackgroundClickHandler(a){if(a=a?a:window.event){var b=a.target?a.target:a.srcElement;b&&("BODY"==b.tagName||"adlytics-adtags-top"==b.id||"banner-region"==b.id||"skyscraper-region"==b.id||"page_margins"==b.id)&&window.open("http://example.org")}}window.onload=jAdInitBackground;
+</script>
