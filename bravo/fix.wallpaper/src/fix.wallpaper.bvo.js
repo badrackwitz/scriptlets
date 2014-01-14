3b248f3d3ebd907a27ef4b24c0c4bae0e675fe07
Gregor Rackwitz <gregor.rackwitz@bauermedia.com>
2014-01-14 09:47:55 +0100

    added fix.wallpaper.bvo scriptlet

---
+++ bravo/fix.wallpaper/src/fix.wallpaper.bvo.js
@@ -0,0 +1,5 @@
+(function(s) {
+  var c=s.join(' ');var e=document.createElement('style');e.type='text/css';if(e.styleSheet)e.styleSheet.cssText=c;else e.appendChild(document.createTextNode(c));document.getElementsByTagName('head')[0].appendChild(e);
+}([
+  '.emnerpz > div { text-align: left !important; }'
+]));
