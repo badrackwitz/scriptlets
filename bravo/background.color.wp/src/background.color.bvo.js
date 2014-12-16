52a54bc3be64d06ac195a12d211fe0b86f98ee63
Gregor Rackwitz <gregor.rackwitz@bauermedia.com>
2014-12-16 10:02:35 +0100

    updated scriptlets for cosmo and bravo background coloring

--- bravo/background.color.wp/src/background.color.bvo.js
+++ bravo/background.color.wp/src/background.color.bvo.js
@@ -1,13 +1,14 @@
 (function(s) {
-		var c=s.join(' ');
-		var e=document.createElement('style');
-		e.type='text/css';
-		if(e.styleSheet)e.styleSheet.cssText=c;
-		else e.appendChild(document.createTextNode(c));
-		document.getElementsByTagName('head')[0].appendChild(e);
+	var c=s.join(' ');
+	var e=document.createElement('style');
+	e.type='text/css';
+	if(e.styleSheet)e.styleSheet.cssText=c;
+	else e.appendChild(document.createTextNode(c));
+	document.getElementsByTagName('head')[0].appendChild(e);
 }
 ([
 	"body { background-color:#000000; }",
-	".dikr-responsive-ads-top { width:940px; text-align:right; background-color:#000000; }",
-	"#wrapper { background-color: #ffffff; }"
+	".dikr-responsive-ads-top { top:-90px; }",
+	"#wrapper { background-color: #ffffff;margin-top: 90px; }",
+	"#wrapper > header { margin-top: 0; }"
 ]));
